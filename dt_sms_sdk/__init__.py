dfcf2c62b94b411628ac83cd900b3cccd7cfd315
Emil <emil.thies@anrufliste.com>
2023-01-22 17:36:42 +0100

    next try

---
+++ dt_sms_sdk/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.0"
+
+from .account import *
+from .iso2_mapper import *
+from .message import *
+from .phone_number import *
+from .pricing import *
+from .sms_api import *
--- setup.py
+++ setup.py
@@ -2,36 +2,22 @@
 
 import os
 
-try:
-    from setuptools import setup
-except ImportError:
-    exit("This package requires Python version >= 3.6 and Python's setuptools")
+import setuptools
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-#with open(os.path.join(here, 'requirements.txt')) as f:
-#    requirements = f.read().splitlines()
-
 
 with open(os.path.join(here, 'README.md')) as f:
     long_description = f.read()
 
-with open(os.path.join(here, 'dt_sms_sdk', 'VERSION')) as f:
-    lines = f.read().splitlines()
-    try:
-        version = [line.split('=')[1] for line in lines if line.startswith('DTSMSAPISDK')][0]
-    except IndexError:
-        exit("Invalid package version in %s" % os.path.join(here, 'dt_sms_sdk', 'VERSION'))
 
-setup(
+setuptools.setup(
     name="unofficial-dt-sms-api-sdk",
-    version=version,
     description="Unofficial Python-SDK for the SMS API of Deutsche Telekom",
     long_description=long_description,
     url="https://github.com/Anrufliste/dt-sms-api-sdk-python",
     author='Emil Thies',
     author_email='uDTSMSAPISDK@anrufliste.com',
-    packages=["dt_sms_sdk"],
     python_requires='>=3.7',
+    packages=setuptools.find_packages(where='dt_sms_sdk'),
     include_package_data=True
 )
